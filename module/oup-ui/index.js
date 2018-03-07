a43fb0df1ea6b0f03a4d5fecabc558f03c9c4556
uzquiano <mgu1@cornell.edu>
2018-03-07 06:16:01 -0500

    Add global flag so that form helper messages appear above field

--- module/oup-ui/index.js
+++ module/oup-ui/index.js
@@ -3,4 +3,7 @@ define(function(require) {
     // action: "create_journal"
     require("./actions/all-journals/create-journal.js");
     
+    // use this to globally control the position of helper text
+    Alpaca.defaultHelpersPosition = "above";
+    
 });
