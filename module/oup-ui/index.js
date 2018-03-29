61d64be1751663ea03515965eefd8f1443a21be3
uzquiano <mgu1@cornell.edu>
2018-03-28 22:42:00 -0400

    updated index

--- module/oup-ui/index.js
+++ module/oup-ui/index.js
@@ -3,7 +3,9 @@ define(function(require) {
     // action: "create-journal"
     require("./actions/create-journal/create-journal.js");
     
-    // use this to globally control the position of helper text
-    Alpaca.defaultHelpersPosition = "above";
+    // action: "edit-journal"
+    require("./actions/create-journal/edit-journal.js");
     
+    // use this to globally control the position of helper text
+    Alpaca.defaultHelpersPosition = "above";    
 });
